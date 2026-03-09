6e225c45eef5a0e6ec7991d03224bb3d9510c4b0
Talha Muhammad <talha_muhammadali@hotmail.com>
2026-03-09 13:56:28 +0500

    [feat] added model selection option

--- utils/llm.py
+++ utils/llm.py
@@ -1,14 +1,17 @@
 """LLM utilities module: together ai and others."""
+from typing import Optional
 from together import AsyncTogether
 
 async def get_together_client():
     client = AsyncTogether()
     return client
 
-async def ainvoke(messages: list[dict[str, str]]):
+async def ainvoke(
+    messages: list[dict[str, str]], model: Optional[str] = "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo"
+) -> dict:
     client = await get_together_client()
     response = await client.chat.completions.create(
-        model="meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
+        model=model,
         messages=messages
     )
     return response
